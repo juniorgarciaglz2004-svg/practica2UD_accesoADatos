95210d97388ef2e77169e34df7fe3b4eba28b1b1
Junior Garcia <clandelosjuegos@gmail.com>
2026-01-11 18:36:37 +0100

    Aplicacion Unidad 2 complentada

---
+++ out/production/PracticaUD2-JuniorGarcia/mysql.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE IF NOT EXISTS practica2ud;
+__
+USE practica2ud;
+__
+CREATE TABLE IF NOT EXISTS producto (
+id_producto  int auto_increment primary key,
+nombre varchar(50) not null,
+descripcion varchar(200) not null,
+estado varchar(50) not null,
+modelo varchar(50) not null,
+marca varchar(50) not null
+);
+__
+CREATE TABLE IF NOT EXISTS empresa (
+id_empresa int auto_increment primary key,
+nombre varchar(50) not null,
+descripcion varchar(200) not null,
+fecha_de_creacion date,
+ubicacion varchar (100) not null,
+valoracion int not null
+);
+__
+CREATE TABLE IF NOT EXISTS kit_educativo (
+id_kit int auto_increment primary key,
+nombre varchar(50) not null,
+descripcion varchar(200) not null,
+cantidad int not null,
+fecha_de_creacion date,
+fecha_de_actualizacion date,
+id_producto int not null,
+id_empresa int not null,
+precio decimal (12,2)not null,
+valoracion int not null
+);
+__
+alter table kit_educativo
+add foreign key (id_producto) references producto(id_producto),
+add foreign key (id_empresa) references empresa(id_empresa);
